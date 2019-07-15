c6a586e8f171f7a3596ea8ea7f4b970b7c3149a1
hyjaidd <1059320200@qq.com>
2019-07-15 16:30:18 +0800

    增加辅料信息

--- src/page/activity/Activity.js
+++ src/page/activity/Activity.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-        <p>activity</p>
+        <p>公司简介</p>
     </div>
   );
 }
--- src/page/info/Info.js
+++ src/page/info/Info.js
@@ -59,9 +59,10 @@ class Index extends React.Component {
                     个人中心
                 </NavBar>
                 <Flex style={{height: "calc(100% - 45px)"}} direction="column" justify="center">
-                    <WingBlank style={{width:'90%'}}>
+                    <WingBlank style={{width:'80%'}}>
                         <Button type="primary">录入农户信息</Button><WhiteSpace />
                         <Button type="primary">录入农资信息</Button><WhiteSpace />
+                        <Button type="primary">录入辅料信息</Button><WhiteSpace />
                     </WingBlank>
                 </Flex>
             </div>
